08fc36091051fac273a7c7cf4ca15ddd2cba4ec1
Oscar Chavez <oscarchb04@gmail.com>
2024-03-29 21:09:10 -0400

    *Add code to handle sendmails

--- HtmlToPdf/Program.cs
+++ HtmlToPdf/Program.cs
@@ -7,18 +7,39 @@ using QuestPDF.Infrastructure;
 
 using HtmlToPdf.Models;
 using HtmlToPdf.Factory;
+using FluentEmail.Core;
+using HtmlToPdf.Settings;
+using FluentEmail.Core.Models;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 QuestPDF.Settings.License = LicenseType.Community;
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+IServiceCollection services = builder.Services;
+IConfiguration configuration = builder.Configuration;
 
-builder.Services.AddRazorTemplating();
-builder.Services.AddSingleton<InvoiceFactory>();
-builder.Services.AddSingleton<PdfGenerateService>();
+services.AddControllers();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+services.AddRazorTemplating();
+services.AddSingleton<InvoiceFactory>();
+services.AddSingleton<PdfGenerateService>();
+
+// fluent email start area
+services.AddOptions<MailSettings>()
+    .BindConfiguration(nameof(MailSettings))
+    .ValidateDataAnnotations();
+
+MailSettings? settings = configuration.GetSection(nameof(MailSettings)).Get<MailSettings>();
+if (settings is null)
+    throw new ArgumentException($"{nameof(MailSettings)} can't be null");
+
+services.AddFluentEmail(settings.DefaultFromEmail)
+    .AddRazorRenderer()
+    .AddSmtpSender(settings.Host, settings.Port, settings.UserName, settings.Password);
+// fluent email end area
 
 var app = builder.Build();
 
@@ -46,12 +67,50 @@ app.MapGet(
     async (
         [FromServices] InvoiceFactory invoiceFactory,
         [FromServices] IRazorTemplateEngine razorTemplateEngine,
-        [FromServices] PdfGenerateService pdfGenerateService) =>
+        [FromServices] PdfGenerateService pdfGenerateService,
+        [FromServices] IFluentEmail fluentEmail,
+        IOptions<MailSettings> mailSettings,
+        CancellationToken cancellationToken) =>
     {
         Invoice invoice = invoiceFactory.Create();
 
         string html = await razorTemplateEngine.RenderAsync("Views/InvoiceReport.cshtml", invoice);
         byte[] pdf = pdfGenerateService.GeneratePdf(html);
+
+        var list = new List<Attachment>(); ;
+        Attachment attach = new()
+        {
+            ContentType = "application/pdf",
+            Data = new MemoryStream(pdf),
+            Filename = $"invoice_{invoice.Number}_{invoice.IssuedDate}.pdf"
+        };
+        list.Add(attach);
+
+        EmailMetadata emailMetadata = new("devflutternet@gmail.com", "Cliente X", string.Empty, string.Empty, "Welcome and confirmation code", html, invoice, list.ToArray());
+
+        MailSettings mailSettingsValue = mailSettings.Value;
+        IFluentEmail fluent = fluentEmail.SetFrom(mailSettingsValue.DefaultFromEmail, mailSettingsValue.DefaultFromName)
+            .Body(emailMetadata.Body);
+
+        if (string.IsNullOrEmpty(emailMetadata.ToName))
+            fluent.To(emailMetadata.To);
+        else
+            fluent.To(emailMetadata.To, emailMetadata.ToName);
+
+        if (!string.IsNullOrEmpty(emailMetadata.Subject))
+            fluent.Subject(emailMetadata.Subject);
+
+        if (!string.IsNullOrEmpty(emailMetadata.CC))
+            fluent.CC(emailMetadata.CC);
+
+        if (!string.IsNullOrEmpty(emailMetadata.Template) && emailMetadata.TemplateModel is not null)
+            fluent.UsingTemplate(emailMetadata.Template, emailMetadata.TemplateModel);
+
+        if (emailMetadata.AttachmentList.Any())
+            foreach (var item in emailMetadata.AttachmentList)
+                fluent.Attach(item);
+
+        await fluent.SendAsync(cancellationToken);
 
         return Results.File(pdf, "application/pdf", $"invoice_{invoice.Number}_{invoice.IssuedDate}.pdf");
     });
@@ -61,3 +120,13 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public sealed record EmailMetadata(
+    string To,
+    string ToName,
+    string Body,
+    string CC,
+    string Subject,
+    string Template,
+    object? TemplateModel,
+    Attachment[] AttachmentList);
---
+++ HtmlToPdf/Settings/MailSettings.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HtmlToPdf.Settings;
+
+public class MailSettings
+{
+
+    [Required]
+    public string DefaultFromEmail { get; set; }
+    [Required]
+    public string DefaultFromName { get; set; }
+    [Required]
+    public string Host { get; set; }
+    [Required]
+    public int Port { get; set; }
+    [Required]
+    public string UserName { get; set; }
+    [Required]
+    public string Password { get; set; }
+}
+
